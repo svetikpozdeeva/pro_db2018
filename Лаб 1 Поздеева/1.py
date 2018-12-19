c5b840d43ecfd890fc4a36b32eb3d2665061efe1
Svetlana <Pozdeeva-1997@mail.ru>
2018-12-19 17:25:11 +0500

    Add files via upload

---
+++ Лаб 1 Поздеева/1.py
@@ -0,0 +1,32 @@
+import random
+from faker import Faker
+import json
+
+f = Faker('ru_RU')
+
+pp = []
+for i in range(100):
+    persona = {}
+    persona['name'] = f.name()
+    persona['email'] = f.email()
+    persona['phone'] = f.phone_number()
+    pp.append(persona)
+
+cc = []
+for i in range(100):
+    company = {}
+    company['name company'] = f.company()
+    cc.append(company)
+
+r = random.randint(1, 100)
+s = random.randint(1, 10000)/100
+d = random.choice(pp)
+g = random.choice(cc)
+
+print(json.dumps(d))
+print(json.dumps(g))
+print(json.dumps(r))
+print(json.dumps(s))
+f = open('persona.json', 'w', encoding='utf8')
+f.write(json.dumps(d) + '\n' + json.dumps(g) + '\n' + json.dumps(r) + '\n' + json.dumps(s))
+f.close()
